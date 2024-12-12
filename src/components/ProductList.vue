c5b1b2e6bf80392a858dc4b73cb702f13e019f81
dwightjack <marco.solazzi@gmail.com>
2024-12-12 10:56:11 +0900

    Initial commit

---
+++ src/components/ProductList.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import { arrayOf } from 'vue-types';
+import ListItem from './ListItem.vue';
+
+defineProps({
+  list: arrayOf(ListItem.props),
+});
+</script>
+<template>
+  <div>
+    <h4>Products list</h4>
+    <ul>
+      <list-item
+        v-for="item in list"
+        :key="item.id"
+        :id="item.id"
+        :description="item.description"
+        :price="item.price"
+        :status="item.status"
+      />
+    </ul>
+  </div>
+</template>
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+import { oneOf } from 'vue-types';
+
+export const statusType = () => oneOf(['available', 'sold out', null] as const);
